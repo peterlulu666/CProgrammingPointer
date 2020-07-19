7b1258d0a81eff6b7ff7b62f34a1a78f7b92221e
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-07-19 09:55:30 -0400

    commit

--- test.c
+++ test.c
@@ -5,9 +5,12 @@ int main() {
     char *greeting = "Hello";
     // Copy address of array to pointer
     // Array is the address of array ans it is the address of the first character
+    // hw is as same as &hw and it is as same as &hw[0]
 //    greeting = &hw[0];
 //    greeting = &hw;
     greeting = hw;
+//    printf("%s, %s\n", &hw[0], greeting);
+//    printf("%s, %s\n", &hw, greeting);
     printf("%s, %s\n", hw, greeting);
     printf("address of hw = %p, %p, address of greeting = %p\n", hw, &hw, greeting);
 
